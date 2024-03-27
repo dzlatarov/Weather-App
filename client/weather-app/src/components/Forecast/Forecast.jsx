19ca6bcd8af255db672cd39ebff3e2bb4d3bc30f
Danail Zlatarov <danzlatarov1@gmail.com>
2024-03-27 21:36:26 +0200

    Add styles to forecast component

--- client/weather-app/src/components/Forecast/Forecast.jsx
+++ client/weather-app/src/components/Forecast/Forecast.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import forecast from '../../apis/forecast'
 import filterForecast from '../../util/filterForecast'
+import daysConverter from '../../util/daysConverter'
+import unixTimestampConvertor from '../../util/unixTimeStampConvertor'
 import './Forecast.css'
 
 const Forecast = (forecastListDumpData) => {
     const [latitude, setLatitude] = useState('')
     const [longtitude, setLongtitute] = useState('')
     const [forecastList, setForecastList] = useState([])
+    let units = localStorage.getItem("unit")
 
     useEffect(() => {
         console.log(forecastListDumpData)
@@ -56,8 +59,16 @@ const Forecast = (forecastListDumpData) => {
 
   return (
     <div className='forecast'>
-          {forecastList && forecastList.length > 0 && forecastList.filter((item, index) => index < 5).map((filteredList, index) =>
-              <div key={index} className='forecastWrapper' onClick={showInDifferentPage}>{filteredList.main.temp.toFixed(0)}</div>
+      {forecastList && forecastList.length > 0 && forecastList.filter((item, index) => index < 5).map((filteredList, index) =>
+        <div key={index} className='forecastWrapper' onClick={showInDifferentPage}>
+              <span className='dateWrapper'>{daysConverter(unixTimestampConvertor(filteredList.dt))}</span>
+              <img src={`https://openweathermap.org/img/wn/${filteredList.weather[0].icon}.png`} className='icon'></img>
+              <span className='temperatureWrapper'>
+                {filteredList.main.temp.toFixed(0)}
+                <span className='symbol'>o</span>
+                {units !== undefined && (<span>{ units === 'metric' ? 'C' : 'F'}</span>)}
+              </span>
+            </div>
           )}
     </div>
   )
