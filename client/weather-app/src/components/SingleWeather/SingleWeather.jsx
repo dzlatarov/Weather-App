841d3f2f3643d7f6cc2bc40b47b6dec8920bae5e
Danail Zlatarov <danzlatarov1@gmail.com>
2024-03-28 16:37:28 +0200

    Display weather by hours and initial submit of SingleWeather css file

--- client/weather-app/src/components/SingleWeather/SingleWeather.jsx
+++ client/weather-app/src/components/SingleWeather/SingleWeather.jsx
@@ -1,28 +1,36 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import './SingleWeather.css'
 import daysConverter from '../../util/daysConverter'
 import { clearForecastData, clearWeatherDetails } from '../../state/weather/weatherSlice'
 import unixTimestampConvertor from '../../util/unixTimeStampConvertor'
+import filterForecastForSingleDay from '../../util/filterForecastForSingleDay'
 
-const SingleWeather = () => {
-  const data = useSelector(state => state.weather.value)
+const SingleWeather = () => { 
+  const currentWeather = useSelector(state => state.weather.value)
+  const forecastList = useSelector(state => state.weather.forecast)
   const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const filteredData = forecastList.length > 0 && currentWeather.length > 0 && filterForecastForSingleDay(forecastList, currentWeather)
+  console.log(`Filtered data from SingleWeather is ${filteredData}`)
 
   useEffect(() => {
+      
     return () => {
+
       dispatch(clearForecastData())
       dispatch(clearWeatherDetails())
-    }
+    };
   },[])
   return (
-    <div>
-      {data && data[0] && (
-        <div className='cardWrapper'>
+    <div className='singleWeatherWrapper'>
+      {filteredData && filteredData.length > 0 && filteredData.map((element, index) =>
+        <div key={index} className='card'>
                 <div className='descriptionWrapper'>
-                   <div className='date'>{daysConverter(unixTimestampConvertor(data[0].dt))}</div>
-                    <img src={`https://openweathermap.org/img/wn/${data[0].weather[0].icon}@2x.png`} className='icon'></img>
-                    <div className='description'>{data[0].weather[0].description}</div>
+                   <div className='date'>{daysConverter(unixTimestampConvertor(element.dt))}</div>
+                    <img src={`https://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png`} className='icon'></img>
+                    <div className='description'>{element.weather[0].description}</div>
                 </div>
                 {/* <div className='temperatureWrapper'>
                       <span>{data[0].temperature.toFixed(0)}</span>
