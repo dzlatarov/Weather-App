d950ea9b419fadf7e65aabd6b8664d215c8c6482
Danail Zlatarov <danzlatarov1@gmail.com>
2024-03-28 16:35:04 +0200

    Fix filterForecastForSingleDay function

--- client/weather-app/src/util/filterForecastForSingleDay.js
+++ client/weather-app/src/util/filterForecastForSingleDay.js
@@ -1,8 +1,22 @@
-import { useSelector } from "react-redux";
+import daysConverter from "./daysConverter";
+import unixTimestampConvertor from "./unixTimeStampConvertor";
 
 const filterForecastForSingleDay = (forecastList, currentWeather) => {
-  console.log(`CurrentWeather dt is ${currentWeather.dt}`);
-  return forecastList.filter((el) => el.dt === currentWeather.dt);
+  let filteredArray = [];
+  if (forecastList !== undefined && currentWeather !== undefined) {
+    let currentDayOfWeek = daysConverter(
+      unixTimestampConvertor(currentWeather[[0]].dt)
+    );
+
+    for (const weather of forecastList) {
+      let day = daysConverter(unixTimestampConvertor(weather.dt));
+      if (day === currentDayOfWeek) {
+        filteredArray.push(weather);
+      }
+    }
+  }
+
+  return filteredArray;
 };
 
 export default filterForecastForSingleDay;
