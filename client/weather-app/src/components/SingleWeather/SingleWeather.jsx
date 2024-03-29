eca1698726cd254a9baa3d3afaa7943efdb21900
Danail Zlatarov <danzlatarov1@gmail.com>
2024-03-29 17:30:24 +0200

    Add media queries for responsive design

--- client/weather-app/src/components/SingleWeather/SingleWeather.jsx
+++ client/weather-app/src/components/SingleWeather/SingleWeather.jsx
@@ -28,21 +28,18 @@ const SingleWeather = () => {
             <h2 className='current-day'>{filteredData && filteredData.length > 0 && daysConverter(unixTimestampConvertor(filteredData[0].dt))}</h2>
             <h2 className='current-date'>{filteredData && utcConverterDate(filteredData[0].dt_txt)}</h2>
           </div>
-          
           <div className='forecast-container'>
-              <div className='forecast-container'>
-              <ul  className='forecast-by-hours'>
-                {filteredData && filteredData.length > 0 && filteredData.map((element, index) =>
-                  <li key={index} className='forecast-element'>
-                    <span>{utcConverter(element.dt_txt).toLocaleTimeString('default', {hour: '2-digit', minute: '2-digit'})}</span>
-                    <img src={`https://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png`}></img>
-                    <span className='description'>{element.weather[0].description}</span>
-                    <span>{element.main.temp.toFixed(0)}°{unit && unit === 'metric' ? "C" : "F"}</span>
-                  </li>
-                  )}
-              </ul>
+                <ul  className='forecast-by-hours'>
+                  {filteredData && filteredData.length > 0 && filteredData.map((element, index) =>
+                    <li key={index} className='forecast-element'>
+                      <span>{utcConverter(element.dt_txt).toLocaleTimeString('default', {hour: '2-digit', minute: '2-digit'})}</span>
+                      <img src={`https://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png`}></img>
+                      <span className='description'>{element.weather[0].description}</span>
+                      <span className='temp'>{element.main.temp.toFixed(0)}°{unit && unit === 'metric' ? "C" : "F"}</span>
+                    </li>
+                    )}
+                </ul>
           </div>
-        </div>
       </div>
     </div>
   )
